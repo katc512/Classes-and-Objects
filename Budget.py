147952f2b62bba2fd6ccc207cff531fdedd09c7f
Kat Coles <katc.512@gmail.com>
2021-04-30 13:58:57 -0500

    Update Budget.py
    Completed Budget version 1

--- Budget.py
+++ Budget.py
@@ -11,18 +11,28 @@ class Budget:
         return "You have successfully deposited ${} into your {} budget.".format(amount, self.category)
 
 
-    def check_balance(self, amount):
-        balance = self.amount
-        return "Your current balance is ${}".format(amount, self.category)
+    def check_balance(self):
+        return "Your current {} balance is ${}.".format(self.category, self.amount)
 
 
     def withdraw(self, amount):
-        self.amount -= amount
-        return "You have succesfully withdrawn ${} out of your {} budget.".format(amount, self.category)
+        if amount <= self.amount:
+            self.amount -= amount
+            return "You have succesfully withdrawn ${} out of your {} budget.".format(amount, self.category)
+
+        else:
+            return "Withdrawal amount exceeds account balance. Please try again."
 
 
-    def transfer(self, amount):
-        pass
+    def transfer(self, amount, category):
+        if self.check_balance():
+            self.amount <= amount
+            self.amount -= amount
+            category.amount += amount
+            return "You have successfully transferred ${} from your {} budget into your {} budget.".format(amount, self.category, category.category)
+
+        else:
+            return "Amount to transfer exceeds account balance. Please try again."
 
 
 food_budget = Budget("FOOD", 325)
@@ -30,5 +40,8 @@ clothing_budget = Budget("CLOTHING", 100)
 entertainment_budget = Budget("ENTERTAINMENT", 150)
 emergency_budget = Budget("EMERGENCY", 500)
 
+
 print(food_budget.deposit(56))
+print(clothing_budget.check_balance())
 print(emergency_budget.withdraw(35))
+print(emergency_budget.transfer(75, food_budget))
